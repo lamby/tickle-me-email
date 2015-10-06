0308b38fef7975ffb66cdb1d575c51e896378cb2
Low Kian Seong <lowks@users.noreply.github.com>
2015-10-07 00:02:28 +0800

    Adding description to setup.py
    Adding `description` to `setup.py` so that it's picked up by `pypi` and project not shown as `UNKNOWN` on `pypi`

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     url='https://chris-lamb.co.uk/projects/tickle-me-email',
     author='Chris Lamb',
     author_email='chris@chris-lamb.co.uk',
+    description='Toolbox for implementing GTD-like behaviours in your IMAP inbox',
     scripts=(
         'tickle-me-email',
     ),
