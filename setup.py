c2d95afbfac5dbe246727c58698cc66592a9b6e4
Chris Lamb <chris@chris-lamb.co.uk>
2019-08-03 14:39:00 +0100

    Releasing version 4.0.2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='tickle-me-email',
-    version='4.0.1',
+    version='4.0.2',
     url='https://chris-lamb.co.uk/projects/tickle-me-email',
     author='Chris Lamb',
     author_email='chris@chris-lamb.co.uk',
